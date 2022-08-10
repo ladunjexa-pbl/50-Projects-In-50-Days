07ae14d869220edaa97837544bc890e234d6f8c7
Liron <110838700+ladunjexa@users.noreply.github.com>
2022-08-10 12:34:03 +0300

    Add files via upload

---
+++ Event-keyCodes/script.js
@@ -0,0 +1,18 @@
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (evt) => {
+    insert.innerHTML = `      
+      <div class="key">
+      ${evt.key === ' ' ? 'Space' : evt.key}
+      <small>event.key</small>
+    </div>
+    <div class="key">
+      ${evt.keyCode}
+      <small>event.keyCode</small>
+    </div>
+    <div class="key">
+      ${evt.code}
+      <small>event.code</small>
+    </div>
+    `
+})
